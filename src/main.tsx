b2a00447f21209542afe8046b13c9a5ed09f0a5f
Benjamin Simmers <bej1380@gmail.com>
2025-01-19 23:05:59 +1000

    update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-// import { Provider } from './app/Provider';
-import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
